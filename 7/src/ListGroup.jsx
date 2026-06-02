1acbb08504eaf7802dcd15b79e97dfa4fb03a8aa
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:05:42 +0300

    Update ListGroup.jsx

--- 7/src/ListGroup.jsx
+++ 7/src/ListGroup.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class ListGroup extends React.Component {
+    render() {
+        const { children } = this.props;
+        return (
+            <ul className="list-group">
+                { React.Children.map(children, (child, i) => {
+                return <li className="list-group-item" key={i}>{child}</li>
+            })}
+            </ul>
+        )
+    }
+}
 // END
