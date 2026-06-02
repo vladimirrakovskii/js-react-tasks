aad0a9aa211dcf48901fc4757fd7ae077bbc8384
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:24:08 +0300

    Update Home.jsx

--- 15/src/Home.jsx
+++ 15/src/Home.jsx
@@ -6,7 +6,16 @@ const content = 'Текст для вкладки Home';
 
 class Home extends React.Component {
   // BEGIN (write your solution here)
+static contextType = ThemeContext;
 
+    render() {
+        const { currentTheme } = this.context;
+        return (
+            <article className={currentTheme.className}>
+                {content}
+            </article>
+        );
+    }
   // END
 }
 
