d77eb42f840374732c01be2230c40f759c20171a
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:01:47 +0300

    Update Progress.jsx

--- 5/src/Progress.jsx
+++ 5/src/Progress.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Progress extends React.Component {
+    render() {
+        const { percentage } = this.props;
+        const progressStyle = {
+            width: percentage + "%",
+        }
+        const ariaValueNow = percentage
+        return (
+            <div className="progress">
+                <div
+                    className="progress-bar"
+                    role="progressbar"
+                    aria-valuenow={ ariaValueNow }
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    aria-label="progressbar"
+                    style={progressStyle}
+                ></div>
+            </div>
+        )
+    }
+}
 // END
