6a7f7288136e5653313b8e208367e29467bddb8f
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 10:57:31 +0300

    Update Card.jsx

--- 3/src/Card.jsx
+++ 3/src/Card.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Card extends React.Component {
+    render() {
+        const {title, text} = this.props;
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">{title}</h4>
+                    <p className="card-text">{text}</p>
+                </div>
+            </div>
+        )
+    }
+}
+Card.defaultProps = {
+    title: 'title',
+    text: 'text',
+}
 // END
