96c92abc5f6e1b8d20a34987fc299e299447c2ea
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:22:03 +0300

    Update Card.jsx

--- 14/src/Card.jsx
+++ 14/src/Card.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+const Title = (props) => <h4 className="card-title">{props.children}</h4>;
+const Body = (props) => <div className="card-body">{props.children}</div>;
+const Text = (props) => <p className="card-text">{props.children}</p>;
+export default class Card extends React.Component {
+    static Body = Body;
+    static Title = Title;
+    static Text = Text;
+    render() {
+        return (
+            <div className="card">{this.props.children}</div>
+        )
+    }
+}
 // END
