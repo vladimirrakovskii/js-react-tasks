560f30bb0b435b466ae0a77056295f883f7cc11f
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:23:35 +0300

    Update App.jsx

--- 15/src/App.jsx
+++ 15/src/App.jsx
@@ -26,7 +26,41 @@ const themes = [
 
 class App extends React.Component {
   // BEGIN (write your solution here)
+constructor(props) {
+    super(props);
+    this.state = {
+      currentTheme: themes[0],
+    };
+  }
 
+  changeTheme = (theme) => {
+    this.setState({ currentTheme: theme });
+  }
+
+  render() {
+    const { currentTheme } = this.state;
+    const contextValue = {
+      currentTheme,
+      themes,
+      changeTheme: this.changeTheme,
+    };
+
+    return (
+        <ThemeContext.Provider value={contextValue}>
+          <div>
+            <ThemeSwitcher />
+            <Tabs defaultActiveKey="home" className="mb-3">
+              <Tab eventKey="home" title="Home">
+                <Home />
+              </Tab>
+              <Tab eventKey="profile" title="Profile">
+                <Profile />
+              </Tab>
+            </Tabs>
+          </div>
+        </ThemeContext.Provider>
+    );
+  }
   // END
 }
 
