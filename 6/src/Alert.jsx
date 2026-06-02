3185978a74ae27863a6490ae5d945b2bd7ba0572
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:04:03 +0300

    Update Alert.jsx

--- 6/src/Alert.jsx
+++ 6/src/Alert.jsx
@@ -2,5 +2,13 @@ import cn from 'classnames';
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Alert extends React.Component {
+    render() {
+        const { type, text } = this.props;
+        const divClass = cn("alert", `alert-${type}`)
+        return (
+            <div className={divClass} role="alert">{text}</div>
+        )
+    }
+}
 // END
