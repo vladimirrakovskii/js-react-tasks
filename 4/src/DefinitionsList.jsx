1ea8651ccee84b242e2592af960577eb0ac4e977
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:00:19 +0300

    Update DefinitionsList.jsx

--- 4/src/DefinitionsList.jsx
+++ 4/src/DefinitionsList.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class DefinitionsList extends React.Component {
+    render() {
+        const { data } = this.props;
+        if (data.length === 0) {
+            return null;
+        }
+        else return (
+            <dl>
+                {data.map(item => (<ListItem dt={item.dt} dd={item.dd} id={item.id} />))}
+            </dl>
+        )
+    }
+}
+class ListItem extends React.Component {
+    render() {
+        const { dt, dd, id } = this.props;
+        return (
+            <>
+                <dt key={ id }>{ dt }</dt>
+                <dd>{ dd }</dd>
+            </>
+        )
+    }
+}
 // END
