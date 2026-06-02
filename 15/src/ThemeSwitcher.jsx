80227b77fbcbed4707b8f92c69728d18180b2ba0
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:25:33 +0300

    Update ThemeSwitcher.jsx

--- 15/src/ThemeSwitcher.jsx
+++ 15/src/ThemeSwitcher.jsx
@@ -5,7 +5,34 @@ import ThemeContext from './contexts';
 
 class ThemeSwitcher extends React.Component {
   // BEGIN (write your solution here)
+static contextType = ThemeContext;
 
+    handleThemeChange = (theme) => {
+        this.context.changeTheme(theme);
+    }
+
+    render() {
+        const { currentTheme, themes } = this.context;
+
+        return (
+            <ButtonGroup className="mb-3">
+                {themes.map((theme) => (
+                    <ToggleButton
+                        key={theme.id}
+                        id={`theme-${theme.id}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="theme"
+                        value={theme.name}
+                        checked={currentTheme.id === theme.id}
+                        onChange={() => this.handleThemeChange(theme)}
+                    >
+                        {theme.name}
+                    </ToggleButton>
+                ))}
+            </ButtonGroup>
+        );
+    }
   // END
 }
 
