30a990a119098b970e207497c07269c1e68af9c3
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:11:53 +0300

    Update Collapse.jsx

--- 10/src/Collapse.jsx
+++ 10/src/Collapse.jsx
@@ -2,5 +2,44 @@ import React from 'react';
 import cn from 'classnames';
 
 // BEGIN (write your solution here)
+export default class Collapse extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: this.props.opened};
+    }
 
+    handleToggle = () => {
+        const { isOpen } = this.state;
+        this.setState({isOpen: !isOpen});
+    }
+
+    render() {
+        const {text} = this.props;
+        const {isOpen} = this.state;
+        const divClass = cn("collapse", {
+            show: isOpen,
+        })
+        return (
+            <div>
+                <p>
+                    <a
+                        className="btn btn-primary"
+                        data-bs-toggle="collapse"
+                        href="#"
+                        role="button"
+                        aria-expanded={isOpen}
+                        onClick={this.handleToggle}
+                    >Link with href</a
+                    >
+                </p>
+                <div className={divClass}>
+                    <div className="card card-body">{text}</div>
+                </div>
+            </div>
+        )
+    }
+}
+Collapse.defaultProps = {
+    opened: true
+}
 // END
