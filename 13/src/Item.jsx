6fb1c885e02df37bca5d46c33b8a4be04a915239
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 11:19:13 +0300

    Update Item.jsx

--- 13/src/Item.jsx
+++ 13/src/Item.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Item extends React.Component {
+    render() {
+        const {task, onRemove} = this.props;
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-primary btn-sm" onClick={onRemove}>-</button>
+                    </div>
+                    <div className="col">{task}</div>
+                </div>
+                <hr/>
+            </div>
+        )
+    }
+}
 // END
